cd9e77217d461474721e0f7d522dfa49c8e72398
Saifuddin <adenwala32@gmail.com>
2024-03-27 17:13:22 +0530

    QUESTION UI created

--- app/src/main/java/com/myprojects/quizapp/Constants.kt
+++ app/src/main/java/com/myprojects/quizapp/Constants.kt
@@ -16,7 +16,115 @@ object Constants {
             "India"
         )
 
+        val question2 = Question(
+            2,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_argentina,
+            "Australia",
+            "Argentina",
+            "Japan",
+            "Pakistan",
+            "Argentina"
+        )
+
+        val question3 = Question(
+            3,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_australia,
+            "Australia",
+            "India",
+            "Japan",
+            "Pakistan",
+            "Australia"
+        )
+
+        val question4 = Question(
+            4,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_belgium,
+            "Australia",
+            "India",
+            "Japan",
+            "Belgium",
+            "Belgium"
+        )
+
+        val question5 = Question(
+            5,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_brazil,
+            "Australia",
+            "India",
+            "Japan",
+            "Brazil",
+            "Brazil"
+        )
+
+        val question6 = Question(
+            6,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_denmark,
+            "Australia",
+            "India",
+            "Japan",
+            "Denmark",
+            "Denmark"
+        )
+
+        val question7 = Question(
+            7,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_fiji,
+            "Australia",
+            "Fiji",
+            "Japan",
+            "Pakistan",
+            "Fiji"
+        )
+
+        val question8 = Question(
+            8,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_germany,
+            "Australia",
+            "India",
+            "Germany",
+            "Pakistan",
+            "Germany"
+        )
+
+        val question9 = Question(
+            9,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_kuwait,
+            "Kuwait",
+            "India",
+            "Japan",
+            "Pakistan",
+            "Kuwait"
+        )
+
+        val question10 = Question(
+            10,
+            "Which county flag it is ?",
+            R.drawable.ic_flag_of_new_zealand,
+            "Australia",
+            "India",
+            "Japan",
+            "New Zealand",
+            "New Zealand"
+        )
+
         questions.add(question1)
+        questions.add(question2)
+        questions.add(question3)
+        questions.add(question4)
+        questions.add(question5)
+        questions.add(question6)
+        questions.add(question7)
+        questions.add(question8)
+        questions.add(question9)
+        questions.add(question10)
 
         return questions
     }
--- app/src/main/java/com/myprojects/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/myprojects/quizapp/QuizQuestionsActivity.kt
@@ -1,15 +1,59 @@
 package com.myprojects.quizapp
 
+import android.annotation.SuppressLint
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var tvProgress: ProgressBar? = null
+    private var progressText: TextView? = null
+    private var questionView: TextView? = null
+    private var imageView: ImageView? = null
+
+    private var option1: TextView? = null
+    private var option2: TextView? = null
+    private var option3: TextView? = null
+    private var option4: TextView? = null
+
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        tvProgress = findViewById(R.id.progressBar)
+        progressText = findViewById(R.id.text_progress)
+        questionView = findViewById(R.id.questionView)
+        imageView = findViewById(R.id.imageView)
+        val buttonSubmit = findViewById<Button>(R.id.btn_submit)
+
+        option1 = findViewById(R.id.option1)
+        option2 = findViewById(R.id.option2)
+        option3 = findViewById(R.id.option3)
+        option4 = findViewById(R.id.option4)
+
         val questionList = Constants.getQuestions()
         Log.i("Question size is ", "${questionList.size}")
+
+        var currentPosition = 1
+
+        buttonSubmit.setOnClickListener {
+            var question = questionList[currentPosition - 1]
+            questionView?.text = question.question
+            imageView?.setImageResource(question.image)
+            progressText?.text = "$currentPosition/10"
+            option1?.text = question.option1
+            option2?.text = question.option2
+            option3?.text = question.option3
+            option4?.text = question.option4
+            currentPosition++
+        }
     }
 }
